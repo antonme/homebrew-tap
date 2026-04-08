0defd2189f13df8ba74daeea281ac956108edd33
Anton Volnuhin <anton@volnuhin.com>
2026-04-08 05:32:21 +0300

    Update mosh formula to fork8-osc11

--- Formula/mosh.rb
+++ Formula/mosh.rb
@@ -1,18 +1,12 @@
 class Mosh < Formula
   desc "Remote terminal application (fork with OSC 10/11 color queries, hyperlinks, SGR dim/strikethrough)"
   homepage "https://github.com/antonme/mosh"
-  url "https://github.com/antonme/mosh/archive/refs/tags/fork7.tar.gz"
-  sha256 "377e4050365274010a52848af82f0380c1959a6d47ae99c42dc564e4ae3fabb3"
-  version "1.4.0-fork7"
+  url "https://github.com/antonme/mosh/archive/refs/tags/fork8-osc11.tar.gz"
+  sha256 "e198af5451ecf14137397a29d8a1f65f0500337f499bfbba1cc1b486257ef43d"
+  version "1.4.0-fork8"
   license "GPL-3.0-or-later"
 
-  bottle do
-    root_url "https://github.com/antonme/homebrew-tap/releases/download/bottles"
-    rebuild 2
-    sha256 cellar: :any, arm64_tahoe: "84d0046ad03ac3e383235301caacdaee8c1778e43a5ae4f20e70c2be951c6840"
-  end
-
-  head "https://github.com/antonme/mosh.git", branch: "osc10-11-query-support"
+  head "https://github.com/antonme/mosh.git", branch: "master"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
